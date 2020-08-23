160d9a55b600f72ec6374d614c96999dba7632aa
yuerino <truongcongthienha@yahoo.com.vn>
2020-08-23 17:03:02 +0200

    feat: add db_schema for upcoming commands

---
+++ constants/db_schema.py
@@ -0,0 +1,66 @@
+GUILD_CONFIG = {
+    '_id': None,
+    'name': None,
+    'icon': None,
+    'banner': None,
+    'visible': True,
+    'reports_enabled': True,
+    'global_blacklisted': False,
+    'global_blacklisted_commands': [],
+    'restricted_commands': [],
+    'events': {
+        'default_enabled': True,
+        'default_mention_id': None,
+        'default_webhook_data': None,
+        'magmaBoss': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'darkAuction': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'bankInterest': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'newYear': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'zoo': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'spookyFestival': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        },
+        'winterEvent': {
+            'enabled': False,
+            'mention_id': None,
+            'channel_id': None,
+            'webhook_data': None
+        },
+        'jerryWorkshopEvent': {
+            'enabled': False,
+            'mention_id': None,
+            'webhook_data': None
+        }
+    },
+    'last_updated': None
+}
+
+PLAYER_DATA = {
+    'mojang_uuids': [],
+    'discord_ids': [],
+    'global_blacklisted': False,
+    'guild_report_blacklisted': [],
+    'guild_reputation_blacklisted': [],
+}
